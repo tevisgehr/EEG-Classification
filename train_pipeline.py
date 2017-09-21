13565f748ddae7e517783da5b450701dfb140f82
Tevis Gehr <tevis.gehr@gmail.com>
2017-09-20 20:33:59 -0600

    fixing for old keras version

--- train_pipeline.py
+++ train_pipeline.py
@@ -203,9 +203,11 @@ y_test = np_utils.to_categorical(y_test, num_classes)
 
 
 model = Sequential()
-model.add(Conv2D(32, (3, 3), padding='same',input_shape=input_shape))
+#model.add(Conv2D(32, (3, 3), padding='same',input_shape=input_shape))
+model.add(Conv2D(32, 3, 3, padding='same',input_shape=input_shape))
 model.add(Activation('relu'))
-model.add(Conv2D(32, (3, 3)))
+#model.add(Conv2D(32, (3, 3)))
+model.add(Conv2D(32, 3, 3))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 #model.add(Dropout(0.25))
